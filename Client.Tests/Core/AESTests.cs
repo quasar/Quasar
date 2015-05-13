287bdd6a891c1512be0ce6b958d2db7515078637
tidusjar <jamierees@outlook.com>
2015-05-13 17:33:42 +0100

    More unit tests around Sha and Aes

--- Client.Tests/Core/AESTests.cs
+++ Client.Tests/Core/AESTests.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Linq;
 using NUnit.Framework;
 using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.NUnit2;
-using Ploeh.AutoFixture.NUnit2.Addins.Builders;
 using xClient.Core.Encryption;
 
 namespace Client.Tests.Core
@@ -47,6 +44,36 @@ namespace Client.Tests.Core
             Assert.That(decrypt, Is.Not.Empty);
             Assert.That(decrypt, Is.EqualTo(value));
             Assert.That(decrypt, Is.Not.EqualTo(encrypt));
+        }
+
+        [Test]
+        public void EncryptAesArray()
+        {
+            var key = Fixture.Create<byte[]>();
+            var value = Fixture.Create<byte[]>();
+
+            var encrypt = AES.Encrypt(value, key);
+
+            Assert.That(encrypt, Is.Not.Null);
+            Assert.That(encrypt, Is.Not.Empty);
+            Assert.That(encrypt, Is.Not.EquivalentTo(value));
+            Assert.That(encrypt, Is.Not.EquivalentTo(key));
+        }
+
+        [Test]
+        public void DecryptAesArray()
+        {
+            var key = Fixture.Create<byte[]>();
+            var value = Fixture.Create<byte[]>();
+
+            var encrypt = AES.Encrypt(value, key);
+
+            var decrypt = AES.Decrypt(encrypt, key).Where(x => x > 0).ToArray();
+
+            Assert.That(decrypt, Is.Not.Null);
+            Assert.That(decrypt, Is.Not.Empty);
+            Assert.That(decrypt, Is.EqualTo(value));
+            Assert.That(decrypt, Is.Not.EquivalentTo(encrypt));
         }
     }
 }
---
+++ Client.Tests/Core/Sha265Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using xClient.Core.Encryption;
+
+namespace Client.Tests.Core
+{
+    [TestFixture]
+    public class Sha256Test
+    {
+        public Fixture Fixture { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Fixture = new Fixture();
+        }
+
+        [Test]
+        public void ComputerHashSuccess()
+        {
+            var input = Fixture.Create<string>();
+            var result = SHA256.ComputeHash(input);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.Not.EqualTo(input));
+        }
+    }
+}
