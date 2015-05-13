b2371a91fac1fd28d2889a94b6da29da7cd5c92e
MaxXor <admin@maxxor.org>
2015-05-13 19:11:55 +0200

    Added unit testing project (#127)

---
+++ ClientTests/Core/Encryption/AES.Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xClient.Core.Encryption;
+using xClient.Core.Helper;
+
+namespace xClientTests.Core.Encryption
+{
+    [TestClass]
+    public class AESTests
+    {
+        [TestMethod]
+        public void EncryptAndDecryptTest()
+        {
+            var input = Helper.GetRandomName(100);
+            var password = Helper.GetRandomName(50);
+            var encrypted = AES.Encrypt(input, password);
+
+            Assert.IsNotNull(encrypted);
+            Assert.AreNotEqual(encrypted, input);
+
+            var decrypted = AES.Decrypt(encrypted, password);
+
+            Assert.IsTrue(input == decrypted);
+        }
+    }
+}
