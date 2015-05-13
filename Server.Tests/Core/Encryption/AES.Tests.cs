e4c8ef73747995b668c734851d0ece2d407d8869
MaxXor <admin@maxxor.org>
2015-05-13 19:31:56 +0200

    Added Server unit testing project

---
+++ Server.Tests/Core/Encryption/AES.Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xServer.Core.Encryption;
+using xServer.Core.Helper;
+
+namespace xServer.Tests.Core.Encryption
+{
+    [TestClass]
+    public class AESTests
+    {
+        [TestMethod]
+        public void EncryptAndDecryptTest()
+        {
+            var input = Helper.GetRandomName(100);
+            var password = Helper.GetRandomName(50);
+            var encrypted = AES.Encrypt(input, password);
+
+            Assert.IsNotNull(encrypted);
+            Assert.AreNotEqual(encrypted, input);
+
+            var decrypted = AES.Decrypt(encrypted, password);
+
+            Assert.IsTrue(input == decrypted);
+        }
+    }
+}
