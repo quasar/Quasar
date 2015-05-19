3990494d47630e97937762545ad496de3524065e
yankejustin <yankejustin@hotmail.com>
2015-05-18 21:01:38 -0400

    Fixed redundant type specification
    Fixed redundant type specification for the server's packets.

--- Server/Core/Packets/ServerPackets/Reconnect.cs
+++ Server/Core/Packets/ServerPackets/Reconnect.cs
@@ -11,7 +11,7 @@ namespace xServer.Core.Packets.ServerPackets
 
         public void Execute(Client client)
         {
-            client.Send<Reconnect>(this);
+            client.Send(this);
         }
     }
 }
--- Server/Core/Packets/ServerPackets/ShellCommand.cs
+++ Server/Core/Packets/ServerPackets/ShellCommand.cs
@@ -19,7 +19,7 @@ namespace xServer.Core.Packets.ServerPackets
 
         public void Execute(Client client)
         {
-            client.Send<ShellCommand>(this);
+            client.Send(this);
         }
     }
 }
