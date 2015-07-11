c1481729761bec411e0433efc3ff4bb0dc986782
MaxXor <admin@maxxor.org>
2015-07-11 19:17:28 +0200

    Reverted background color of Builder
    - changed from White to SystemColors.Control

--- Server/Forms/FrmBuilder.Designer.cs
+++ Server/Forms/FrmBuilder.Designer.cs
@@ -683,7 +683,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(673, 499);
             this.Controls.Add(this.groupAdditional);
             this.Controls.Add(this.groupAsmInfo);
