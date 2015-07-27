b0fd538a4e91706dd0cf05cfcc4510e63be38725
yankejustin <yankejustin@hotmail.com>
2015-07-27 18:17:43 -0400

    Added the basic Registry Editor Interface

---
+++ Server/Forms/FrmRegistryEditor.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace xServer.Forms
+{
+    partial class FrmRegistryEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRegistryEditor));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(4, 5);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.ShowPlusMinus = false;
+            this.treeView1.Size = new System.Drawing.Size(155, 402);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(163, 5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(526, 402);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // FrmRegistryEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 427);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(710, 465);
+            this.Name = "FrmRegistryEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "xRAT 2.0 - Registry Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ Server/Forms/FrmRegistryEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using xServer.Core.Networking;
+
+namespace xServer.Forms
+{
+    public partial class FrmRegistryEditor : Form
+    {
+        private readonly Client _connectClient;
+
+        public FrmRegistryEditor(Client c)
+        {
+            _connectClient = c;
+            _connectClient.Value.FrmRe = this;
+
+            InitializeComponent();
+        }
+    }
+}
