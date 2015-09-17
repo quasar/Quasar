d66a71754b15729878908ac3562bbc256680dece
d3agle <sellout89@gmail.com>
2015-09-17 03:56:50 -0500

    Small revisions

--- Server/Controls/RapidPictureBox.cs
+++ Server/Controls/RapidPictureBox.cs
@@ -92,6 +92,8 @@ namespace xServer.Controls
         /// </summary>
         public void Start()
         {
+            _frameCounter = new FrameCounter();
+
             _sWatch = Stopwatch.StartNew();
 
             Running = true;
@@ -149,8 +151,6 @@ namespace xServer.Controls
         public RapidPictureBox()
         {
             this.SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
-
-            _frameCounter = new FrameCounter();
         }
 
         protected override CreateParams CreateParams
