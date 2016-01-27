15388c3fff8d849fe00a05cb0abcbccb934f7411
StingRaptor <develop.state@gmail.com>
2016-01-27 09:18:56 +0100

    Added client and server packets to allow modification

---
+++ Client/Core/Packets/ClientPackets/GetChangeRegistryValueResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xClient.Core.Networking;
+using xClient.Core.Registry;
+
+namespace xClient.Core.Packets.ClientPackets
+{
+    [Serializable]
+    public class GetChangeRegistryValueResponse : IPacket
+    {
+        public string KeyPath { get; set; }
+        public RegValueData Value { get; set; }
+
+        public bool IsError { get; set; }
+        public string ErrorMsg { get; set; }
+
+        public GetChangeRegistryValueResponse() { }
+
+        public void Execute(Client client)
+        {
+            client.Send(this);
+        }
+    }
+}
