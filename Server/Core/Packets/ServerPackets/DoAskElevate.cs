6995b04cbd9c67d83ebd88354811f721d946a483
LostSoulFly <Bradley.Shaner@gmail.com>
2016-03-18 12:07:57 -0400

    Try not to make it so obvious that this is my second time working with c#

--- Server/Core/Packets/ServerPackets/DoAskElevate.cs
+++ Server/Core/Packets/ServerPackets/DoAskElevate.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using xServer.Core.Networking;
-using xServer.Core.Registry;
 
 namespace xServer.Core.Packets.ServerPackets
 {
     [Serializable]
     public class DoAskElevate : IPacket
     {
-        public DoAskElevate(string keyPath, RegValueData value)
+        public DoAskElevate()
         {
         }
 
