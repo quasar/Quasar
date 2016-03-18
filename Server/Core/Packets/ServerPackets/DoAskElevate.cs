da42bd3e0ca81ccff41384e281c56573304cc51f
LostSoulFly <Bradley.Shaner@gmail.com>
2016-03-18 12:04:58 -0400

    Create DoAskElevate ServerPacket

---
+++ Server/Core/Packets/ServerPackets/DoAskElevate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using xServer.Core.Networking;
+using xServer.Core.Registry;
+
+namespace xServer.Core.Packets.ServerPackets
+{
+    [Serializable]
+    public class DoAskElevate : IPacket
+    {
+        public DoAskElevate(string keyPath, RegValueData value)
+        {
+        }
+
+        public void Execute(Client client)
+        {
+            client.Send(this);
+        }
+    }
+}
