c5ac31eff6ff411176da0478ce54f3c84561b1ef
LjungErik <develop.state@gmail.com>
2016-04-22 10:13:36 +0200

    Improved code for 'FrmRegValueEditMultiString'
    * Removed unnecessary close and load code
    * Removed unnecessary string sanitizing

--- Server/Forms/FrmRegValueEditMultiString.Designer.cs
+++ Server/Forms/FrmRegValueEditMultiString.Designer.cs
@@ -31,11 +31,9 @@
             this.valueNameTxtBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
             this.valueDataTxtBox = new System.Windows.Forms.TextBox();
-            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // valueNameTxtBox
@@ -47,7 +45,7 @@
             this.valueNameTxtBox.Name = "valueNameTxtBox";
             this.valueNameTxtBox.ReadOnly = true;
             this.valueNameTxtBox.Size = new System.Drawing.Size(346, 20);
-            this.valueNameTxtBox.TabIndex = 5;
+            this.valueNameTxtBox.TabIndex = 3;
             // 
             // label1
             // 
@@ -58,7 +56,6 @@
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 13);
-            this.label1.TabIndex = 4;
             this.label1.Text = "Value name:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -71,41 +68,27 @@
             this.label2.Location = new System.Drawing.Point(12, 53);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 6;
             this.label2.Text = "Value data:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.flowLayoutPanel1.Controls.Add(this.cancelButton);
-            this.flowLayoutPanel1.Controls.Add(this.okButton);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(24, 327);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(337, 29);
-            this.flowLayoutPanel1.TabIndex = 8;
             // 
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(259, 3);
+            this.cancelButton.Location = new System.Drawing.Point(286, 330);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 4;
+            this.cancelButton.TabIndex = 2;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(178, 3);
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(205, 330);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 5;
+            this.okButton.TabIndex = 1;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
             this.okButton.Click += new System.EventHandler(this.okButton_Click);
@@ -113,12 +96,12 @@
             // valueDataTxtBox
             // 
             this.valueDataTxtBox.AcceptsReturn = true;
-            this.valueDataTxtBox.Location = new System.Drawing.Point(15, 69);
+            this.valueDataTxtBox.Location = new System.Drawing.Point(15, 72);
             this.valueDataTxtBox.Multiline = true;
             this.valueDataTxtBox.Name = "valueDataTxtBox";
             this.valueDataTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.valueDataTxtBox.Size = new System.Drawing.Size(346, 252);
-            this.valueDataTxtBox.TabIndex = 9;
+            this.valueDataTxtBox.TabIndex = 0;
             this.valueDataTxtBox.WordWrap = false;
             // 
             // FrmRegValueEditMultiString
@@ -128,8 +111,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
             this.ClientSize = new System.Drawing.Size(373, 365);
+            this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.valueDataTxtBox);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.okButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.valueNameTxtBox);
             this.Controls.Add(this.label1);
@@ -139,8 +123,6 @@
             this.Name = "FrmRegValueEditMultiString";
             this.ShowIcon = false;
             this.Text = "Edit Multi-String";
-            this.Load += new System.EventHandler(this.FrmRegValueEditMultiString_Load);
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,7 +133,6 @@
         private System.Windows.Forms.TextBox valueNameTxtBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button okButton;
         private System.Windows.Forms.TextBox valueDataTxtBox;
