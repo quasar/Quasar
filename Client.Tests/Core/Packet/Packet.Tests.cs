397da1c965cd1f6cba27a6bd00ee0758d814f910
abdullah2993 <a.saleem2993@gmail.com>
2016-07-27 13:17:04 +0500

    Tests added for packet registeration for serialization

---
+++ Client.Tests/Core/Packet/Packet.Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Linq;
+using xClient.Core.Packets;
+
+namespace xClient.Tests.Core.Packet
+{
+    [TestClass]
+    public class PacketTest
+    {
+        [TestMethod]
+        public void AreAllPacketsRegistered()
+        {
+            var asm = Assembly.Load("Client");
+            var expectedPacketTypes = asm.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IPacket)) && t.GetCustomAttributes(typeof(SerializableAttribute), false).Any()).ToArray();
+            var registeredPackets = PacketRegistery.GetPacketTypes();
+            CollectionAssert.AreEquivalent(expectedPacketTypes, registeredPackets);
+        }
+    }
+}
