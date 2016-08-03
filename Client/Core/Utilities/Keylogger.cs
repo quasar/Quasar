144922ad0411663e24de36393fc7f8466c7c6067
d3agle <d3agle@gmx.com>
2016-08-03 09:35:38 -0500

    unused namespace removed

--- Client/Core/Utilities/Keylogger.cs
+++ Client/Core/Utilities/Keylogger.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Forms;
 using xClient.Core.Helper;
