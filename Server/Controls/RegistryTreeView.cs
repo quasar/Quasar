546719debd36fe83e5671621d8d1a8c375f6f446
d3agle <d3agle@gmx.com>
2016-08-12 14:22:33 -0500

    Fixed flickering in registrytree nodes

--- Server/Controls/RegistryTreeView.cs
+++ Server/Controls/RegistryTreeView.cs
@@ -8,11 +8,20 @@ namespace xServer.Controls
 {
     public class RegistryTreeView : TreeView
     {
-
         public RegistryTreeView()
         {
             //Enable double buffering and ignore WM_ERASEBKGND to reduce flicker
             SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams handleParam = base.CreateParams;
+                handleParam.ExStyle |= 0x02000000;// WS_EX_COMPOSITED       
+                return handleParam;
+            }
         }
     }
 }
