e47fe4a15c76228a05fca9ad22918df9825d9a8e
ubbelol <ubbelolhf@gmail.com>
2016-08-15 15:15:41 +0200

    Fix memory leak in quicklz_helper

--- NativeClient/include/quicklz_helper.cpp
+++ NativeClient/include/quicklz_helper.cpp
@@ -5,8 +5,12 @@
 using namespace std;
 
 void quicklz_helper::compress_data(vector<char> &data) {
-	int32_t finalSize = qlz_size_compressed(&data[0]);
-
+	qlz_state_compress *state_compress = static_cast<qlz_state_compress *>(malloc(sizeof(qlz_state_compress)));
+	char *dest = new char[data.size() + 400];
+	auto len = qlz_compress(&data[0], dest, data.size(), state_compress);
+	data.resize(len);
+	memcpy(&data[0], dest, len);
+	delete[] dest;
 }
 
 void quicklz_helper::compress_data(vector<unsigned char> &data) {
@@ -15,6 +19,7 @@ void quicklz_helper::compress_data(vector<unsigned char> &data) {
 	auto len = qlz_compress(reinterpret_cast<char*>(&data[0]), dest, data.size(), state_compress);
 	data.resize(len);
 	memcpy(&data[0], dest, len);
+	delete[] dest;
 }
 
 void quicklz_helper::decompress_data(vector<char> &data) {
