fea5c12e3e662d6b43bbb0d45b2a07d86fdf9904
MaxXor <admin@maxxor.org>
2018-10-22 17:30:03 +0200

    Remove unnecessary client resource

--- Quasar.Client/Properties/Resources.Designer.cs
+++ Quasar.Client/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Quasar.Client.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap information {
-            get {
-                object obj = ResourceManager.GetObject("information", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
