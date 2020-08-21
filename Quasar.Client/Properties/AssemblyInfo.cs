52ff39d7f746bb6bd820e990ed2c6fcbc6a45486
Hristiyan Ivanov <hrisivanov2@gmail.com>
2020-08-22 01:34:32 +0300

    MS Edge & Brave Password Recovery

--- Quasar.Client/Properties/AssemblyInfo.cs
+++ Quasar.Client/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.2")]
+[assembly: AssemblyFileVersion("1.4.2")]
