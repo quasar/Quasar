d6da8e18e2427764e3c8be77e3239e3c260cf05e
Hristiyan Ivanov <hrisivanov2@gmail.com>
2020-08-26 00:44:01 +0300

    Revert Version

--- Quasar.Common.Tests/Properties/AssemblyInfo.cs
+++ Quasar.Common.Tests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("cfda6d2e-8ab3-4349-b89a-33e1f0dab32b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2")]
-[assembly: AssemblyFileVersion("1.4.2")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
