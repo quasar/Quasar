9d6c51fc39be3b3fa9d5fba7f79d31cebe4d3a4b
UserWithoutAuth <idan40009@gmail.com>
2021-11-27 01:23:03 +0200

    Add Telegram Bot Support

--- Quasar.Server/Forms/FrmMain.cs
+++ Quasar.Server/Forms/FrmMain.cs
@@ -534,12 +534,6 @@ namespace Quasar.Server.Forms
             return result;
         }
 
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-
         public static string Base64Decode(string base64EncodedData)
         {
             var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
